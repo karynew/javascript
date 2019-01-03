232eaf0f1478c6dbb9e8ddf1ad6eb3fc87c7487f
karynew <karynew@gmail.com>
2019-01-03 17:42:02 -0500

    my first files

---
+++ app.js
@@ -0,0 +1,57 @@
+//let nombre = prompt('cual es tu nombre');
+//let edad = prompt('¿cual es tu edad?');
+
+//document.getElementById('app').innerHTML = `Bienvenido ${nombre} de ${edad} años`;
+
+let nombre , mensaje;
+    producto = "juego";
+
+//concatenar javascript
+
+mensaje= 'Java'+ 'script';
+
+let aprendiendo = 'Aprendiendo',
+    tecnologia = ' Javascript';
+
+let message = 'Aprendiendo javascript, css html para ser FrontEnd';
+
+//console.log(`${aprendiendo} ${tecnologia}` );
+
+console.log(tecnologia.length);
+
+//concat
+
+console.log(tecnologia.concat(' ', 'Es genial'));
+
+// toUpperCase
+
+console.log(tecnologia.toUpperCase());
+
+// toLowerCase
+
+console.log(tecnologia.toLowerCase());
+
+//indexOf
+
+console.log(message.indexOf('javascript'));
+
+//substring
+console.log(message.substring(0,11));
+
+//slice
+console.log(message.slice(-3));
+
+//split
+
+console.log(message.split(' '));
+
+//replace
+
+console.log(message.replace('css', 'PHP'));
+
+//includes
+
+console.log(message.includes('css'));
+
+//repeat
+console.log(tecnologia.repeat(10));
