ec6936329af2ff816cc0bebfc212a4659ee31c09
karina <karynew@gmail.com>
2019-01-03 22:32:34 -0500

    numbers on javascript

---
+++ numeros.js
@@ -0,0 +1,46 @@
+//numeros javascript
+
+const numero1 = 30,
+      numero2 = 20,
+      numero3 = 20.20,
+      numero4 = .1020,
+      numero5 =-3;
+
+
+      let resultado;
+      //suma
+
+      resultado = numero1+numero2;
+      //resta
+      resultado = numero1-numero2;
+      //multiplicar
+      resultado = numero1*2;
+      //divisiones
+      resultado = numero1 / numero2;
+      //modulo lo que queda de una division
+      resultado = numero1 % numero2;
+      // pi
+      resultado = Math.PI;
+      //round
+      resultado = Math.round(2.4);
+      //redondea hacia arriba
+      resultado = Math.ceil(2.4);
+      //redondea hacia abajo
+      resultado = Math.floor(2.4);
+      // raiz cuadrada
+      resultado = Math.sqrt(144);
+      //absoluto
+      resultado = Math.abs(numero5);
+      //potencia
+      resultado = Math.pow(8,3);
+      //minimo
+      resultado = Math.min(3,5,6,9,7,8);
+      //maximo
+      resultado = Math.max(3,5,6,9,7,8);
+      //aleatorio
+      resultado = Math.random();
+      resultado = (10 + 20 ) * 5;
+      resultado = (10+20+20+10+40)*.20;
+      let puntaje = 10;
+
+      console.log(puntaje);
