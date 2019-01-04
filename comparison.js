0954036838e44168a2a9760d8aa1dd774c84b1d1
karina <karynew@gmail.com>
2019-01-03 23:11:10 -0500

    datajs and comparison.js

---
+++ comparison.js
@@ -0,0 +1,10 @@
+// operadores de comparacion
+
+const numero1 =20;
+const numero2 = 50;
+const numero3 ="20";
+
+console.log(numero1 > numero2);
+
+//comparador de igual
+console.log(20 == 20);
---
+++ data.js
@@ -0,0 +1,24 @@
+// tipos de datos en javascript
+// datos primitivos
+let valor;
+
+valor ='cadena de texto';
+valor = 20;
+valor = true;
+valor = null;
+valor = undefined;
+
+//symbols
+valor = Symbol('simbolo');
+
+//datos de referencia
+//objeto
+valor = {
+    nombre:'juan'
+}
+//array
+//date
+valor = new Date();
+
+
+console.log(typeof(valor));
